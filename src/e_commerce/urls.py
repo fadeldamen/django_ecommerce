17dea485ae82f21716edb46574e8249d280fe9e4
Fadel Damen Schreiner <fadel.damen@gmail.com>
2020-04-11 09:57:48 -0400

    novo

--- src/e_commerce/urls.py
+++ src/e_commerce/urls.py
@@ -21,5 +21,7 @@ urlpatterns = [
     path('', home_page),
     path('about/', about_page),
     path('contact/', contact_page),
+    path('login/', login_page),
+    path('register/', register_page),
     path('admin/', admin.site.urls),
 ]
